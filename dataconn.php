50c226fe4faf64a9a6179bbf4516f949610af42b
amandeep <amandeepsingh.zapbuild@gmail.com>
2020-01-21 14:59:26 +0530

    initial commit

---
+++ dataconn.php
@@ -0,0 +1,16 @@
+ <html>
+ <head></head>
+ <body>
+ <?php
+     $servername = "localhost";
+      $username = "root";
+       $password = "1234";
+   $conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+                      die("Connection failed: " . $conn->connect_error);
+           }
+     echo "Connected successfully";
+
+     ?> 
+   </body>
+</html>
